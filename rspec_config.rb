a6744adefcb158e73a00a3655d7f257b56df317f
Glenn <gcrosby15@email.com>
2015-05-10 15:02:41 +0700

    Complete 00_hello

--- rspec_config.rb
+++ rspec_config.rb
@@ -2,3 +2,10 @@ RSpec.configure do |c|
   c.fail_fast = true
   c.color = true
 end
+
+# Added this to turn off "should" deprication warnings.
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
